fdb4dd189a4fbc0c215611e4e764efa9bd1ebbaf
Mark Williams <markrwilliams@gmail.com>
2013-08-12 17:07:33 -0700

    proper node padding

--- simple.js
+++ simple.js
@@ -2,7 +2,7 @@ simple = (function () {
     var self = {};
     var height = 400, width = 600;
 
-    offset = 50;
+    var offset = 100;
 
     var env = {};
     self.env = env;
@@ -15,7 +15,7 @@ simple = (function () {
     env.A = {bases: [env.B, env.C]};
     var determine_rank = function (c) {
         if (!c.bases.length)
-            return 0
+            return 0;
         return 1 + Math.min.apply(null, c.bases.map(determine_rank));
     };
 
@@ -32,7 +32,7 @@ simple = (function () {
 
 
     classes.forEach(function (c) {
-        c.rank = offset + c.rank * height / classes.length
+        c.rank = offset + c.rank * height / classes.length;
     });
 
     var add_arrow = function (args) {
@@ -51,10 +51,13 @@ simple = (function () {
     self.svg = d3.select('body').append('svg')
         .attr('width', width)
         .attr('height', height);
-    add_arrow({svg: self.svg, id: 'end-arrow'})
+
+    add_arrow({svg: self.svg, id: 'end-arrow'});
 
     var link = self.svg.selectAll('line').data(edges);
     var circle = self.svg.selectAll('circle').data(classes);
+    var radius = 20;
+
 
     link.enter().append('line')
         .attr('stroke', 'black')
@@ -63,21 +66,29 @@ simple = (function () {
     circle.enter().append('circle')
         .style('stroke', 'black')
         .style('fill', 'white')
-        .attr('r', 10);
+        .attr('r', radius);
+
+    var hypotenuse = function (point_a, point_b) {
+        var leg1 = point_a.x - point_b.x, leg2 = point_a.y - point_b.y;
+        return Math.sqrt(leg1 * leg1 + leg2 * leg2);
+    };
+
+    var translate = function (a, b, hypotenuse, fuzz) {
+        var perimeter_point = radius + (fuzz || 5);
+        return b + (perimeter_point * (a - b) / hypotenuse);
+    };
 
     self.force = d3.layout.force()
         .nodes(classes)
         .links(edges)
         .size([width, height])
-        .charge(-1000)
-        .linkDistance(10)
+        .charge(-5000)
+        .linkDistance(20)
         .on('tick', function (e) {
-
-
             link.attr('x1', function(d) { return d.source.x; });
             link.attr('y1', function(d) { return d.source.y; });
-            link.attr('x2', function(d) { return d.target.x; });
-            link.attr('y2', function(d) { return d.target.y; });
+            link.attr('x2', function(d) { return translate(d.source.x, d.target.x, hypotenuse(d.source, d.target)); });
+            link.attr('y2', function(d) { return translate(d.source.y, d.target.y, hypotenuse(d.source, d.target)); });
             link.attr('marker-end', 'url(#end-arrow)');
 
             circle.attr('cx', function (d) { return d.x += (width / 2 - d.x) * e.alpha; });
